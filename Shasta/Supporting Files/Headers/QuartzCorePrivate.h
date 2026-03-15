413b480e8596eb198a4947ee954725ddadbd9aa2
Fisher Lee <fisherlee0820@outlook.com>
2026-03-15 23:41:49 +0800

    优化QuartzCorePrivate.h
    优化QuartzCorePrivate.h

--- Shasta/Supporting Files/Headers/QuartzCorePrivate.h
+++ Shasta/Supporting Files/Headers/QuartzCorePrivate.h
@@ -1,4 +1,3 @@
-
 //
 //  QuartzCorePrivate.h
 //  Appearance Maker
@@ -10,21 +9,20 @@
 @import Cocoa;
 @import QuartzCore;
 
+extern NSData *CAEncodeLayerTree(CALayer *rootLayer);
+extern NSString *kCAPackageTypeArchive;
+extern NSString *kCAPackageTypeCAMLBundle;
+
 @interface CAFilter: NSObject<NSCopying, NSMutableCopying, NSCoding>
 + (instancetype)filterWithType:(NSString *)type;
 + (NSArray <NSString *> *)filterTypes;
 - (NSArray <NSString *> *)outputKeys;
 - (NSArray <NSString *> *)inputKeys;
 - (void)setDefaults;
-
 @property(copy) NSString *name;
 @property(readonly) NSString *type;
 @property(getter=isEnabled) BOOL enabled;
 @end
-
-extern NSData *CAEncodeLayerTree(CALayer *rootLayer);
-extern NSString *kCAPackageTypeArchive;
-extern NSString *kCAPackageTypeCAMLBundle;
 
 @interface CAPackage: NSObject
 + (id)packageWithData:(NSData *)data type:(NSString *)type options:(id)opts error:(NSError **)outError;
