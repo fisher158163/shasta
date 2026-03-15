dd9b1b4edcfe427925de0616240787b6fae22667
Fisher Lee <fisherlee0820@outlook.com>
2026-03-15 23:35:35 +0800

    优化代码
    优化代码

--- Shasta/Supporting Files/Headers/QuartzCore.h
+++ Shasta/Supporting Files/Headers/QuartzCore.h
@@ -10,40 +10,31 @@
 @import Cocoa;
 @import QuartzCore;
 
-@interface CAFilter : NSObject <NSCopying, NSMutableCopying, NSCoding>
-
+@interface CAFilter: NSObject<NSCopying, NSMutableCopying, NSCoding>
 + (instancetype)filterWithType:(NSString *)type;
 + (NSArray <NSString *> *)filterTypes;
-
 - (NSArray <NSString *> *)outputKeys;
 - (NSArray <NSString *> *)inputKeys;
 - (void)setDefaults;
 
 @property(copy) NSString *name;
 @property(readonly) NSString *type;
-
 @property(getter=isEnabled) BOOL enabled;
-
 @end
 
 extern NSData *CAEncodeLayerTree(CALayer *rootLayer);
-
 extern NSString *kCAPackageTypeArchive;
 extern NSString *kCAPackageTypeCAMLBundle;
 
-@interface CAPackage : NSObject
-
+@interface CAPackage: NSObject
 + (id)packageWithData:(NSData *)data type:(NSString *)type options:(id)opts error:(NSError **)outError;
 + (id)packageWithContentsOfURL:(NSURL *)url type:(NSString *)type options:(id)opts error:(NSError **)outError;
-
 - (NSArray <NSString *> *)publishedObjectNames;
-
 @property(readonly, getter=isGeometryFlipped) BOOL geometryFlipped;
 @property(readonly) CALayer *rootLayer;
-
 @end
 
-@interface CAStateController : NSObject;
+@interface CAStateController: NSObject;
 @property (readonly) CALayer* layer; 
 -(void)setState:(id)arg1 ofLayer:(id)arg2 transitionSpeed:(float)arg3 ;
 -(void)setState:(id)arg1 ofLayer:(id)arg2 ;
@@ -53,8 +44,7 @@ extern NSString *kCAPackageTypeCAMLBundle;
 -(void)_applyTransition:(id)arg1 layer:(id)arg2 undo:(id)arg3 speed:(float)arg4 ;
 @end
 
-
-@interface CAState : NSObject;
+@interface CAState: NSObject;
 @end
 
 @interface CALayer (CAState)
